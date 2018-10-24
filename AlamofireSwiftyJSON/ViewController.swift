c4c0336f015cc2d7845e5825fce7d240d0c74a7a
tayseerAnwar5 <tayseer.ibrahim@pentavalue.com>
2018-10-24 20:58:02 +0200

    commit -m "first commit"

---
+++ AlamofireSwiftyJSON/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  AlamofireSwiftyJSON
+//
+//  Created by Nervana on 10/24/18.
+//  Copyright © 2018 Nervana. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableJSON: UITableView!
+    var arrNames = [[String:AnyObject]]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Alamofire.request("http://api.androidhive.info/contacts/").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                if let resData = swiftyJsonVar["contacts"].arrayObject {
+                    self.arrNames = resData as! [[String:AnyObject]]
+                }
+                if self.arrNames.count > 0 {
+                    self.tableJSON.reloadData()
+                }
+            }
+        }
+    }
+}
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrNames.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "jsonCell")!
+        var dictonary = arrNames[indexPath.row]
+        cell.textLabel?.text = dictonary["name"] as? String
+        return cell
+    }
+}
